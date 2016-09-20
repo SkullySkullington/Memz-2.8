3bff1e4457a0b7f2d07cab803a7068898286ab9d
SkullySkullington <theemocaptinsparkelz@gmail.com>
2016-09-20 08:24:13 -0400

    Add files via upload

---
+++ Welcome.bat
@@ -0,0 +1,9 @@
+cls
+echo Welcome to Memz v2.8
+pause
+If you would like to install hit continue
+pause
+Before installing this program i must warn you i am not responisble for what happens to your computer after this installer are you 100% sure you would like to continue?
+pause
+Alright, dont say i didnt warn you -_-
+pause
